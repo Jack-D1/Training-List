f50884b78a50cb24a53249bbce70cf2341b50c16
Jack Donnelly <jack@jackdonnellyphotography.com>
2018-04-08 11:39:58 +0100

    Fixed Login page and script
    Login page didnt have submit button and login script was having isdsues with header information, common problem, solved by making the redirect the last thing in the script

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 include("connection.php");
 
 session_start();
-
+$goodLogin = 0;
 $uName = $_POST['uName'];
 $pWord = $_POST['pWord'];
 
@@ -12,11 +12,18 @@ if(mysqli_num_rows($checkInDatabase) > 0){
     $accDetails = mysqli_fetch_assoc($checkInDatabase);
     if (password_verify($pWord, $accDetails['password'])){
         $_SESSION['username'] = $uName;
-        header("Location home.php");
+		$goodLogin = 1;
     }else{
         echo "Incorrect password, please try again";
+		$goodLogin = 0;
     }
 }else{
     echo "Your account cannot be found please try again";
+	$goodLogin = 0;
+}
+
+
+if($goodLogin == 1){
+	header("Location: home.php");
 }
 ?>

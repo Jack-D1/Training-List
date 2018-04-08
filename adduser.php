29034790ca70140dcc4782454841759401b9b1ca
Jack Donnelly <jack@jackdonnellyphotography.com>
2018-04-08 11:30:51 +0100

    Fixed password hashing

--- adduser.php
+++ adduser.php
@@ -5,9 +5,9 @@ if(isset($_POST['NewUsername'])){
     $options = [
         'cost' => 15,
     ];
-    $Password = password_hash($_POST['NewPassword'], PASSWORD_DEFAULT); 
+    $Password = password_hash($_POST['NewPassword'], PASSWORD_BCRYPT. $options); 
     $uname = $_POST['NewUsername']; 
     mysqli_query($connection,"INSERT INTO account VALUES('$uname', '$Password')");
-//}
+}
 
 ?>

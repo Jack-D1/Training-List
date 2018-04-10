c90d74d526052271c51daff6df0c51fa1f8b11b4
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-04-10 08:31:18 +0100

    Added add employee page

--- AddEmployee.php
+++ AddEmployee.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 include("check.php");
 include("connection.php");
 ?>
@@ -11,6 +11,98 @@ include("connection.php");
     <title>Add Employee</title>
 </head>
 <body>
-
+    <form method="post" onsubmit="addEmp();">
+        <table>
+            <tr>
+                <td>Name</td>
+                <td>
+                    <input type="text" name="name" id="name" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Clock Number</td>
+                <td>
+                    <input type="text" name="clockno" id="clock" value="" required />
+                </td>
+            </tr>
+            <?php
+            if ($_SESSION['UserID'] > 2){
+                echo '<input type="hidden" name="dept" id="dept" value = '.$_SESSION['dept'].' required />';
+            }else {
+            ?>
+            <tr>
+                <td>Department</td>
+                <td>
+                    <input type="text" name="dept" id="dept" required />
+                </td>
+            </tr>
+            <?php
+            }
+            ?>
+            <tr>
+                <td>Trainer</td>
+                <td>
+                    <input type="text" name="trainer" id="train" value="0" required />
+                </td>
+            </tr>
+            <?php
+            if($_SESSION['UserID'] > 2){
+                echo '<input type="hidden" name="manag" id="manag" value="0" required />';
+            }else{
+            ?>
+            <tr>
+                <td>Manager</td>
+                <td>
+                    <input type="text" name="manag" id="manag" value="0" required />
+                </td>
+            </tr>
+            <?php   
+            }
+            ?>
+            <tr>
+                <td>
+                    <input type="submit" id="name" value="Submit" />
+                </td>
+            </tr>
+        </table>
+    </form>
 </body>
 </html>
+
+<script type="text/javascript" src="jquery.min.js"></script>
+
+
+<script type="text/javascript">
+
+
+    function addEmp() {
+            event.preventDefault();
+
+            var name = document.getElementById("name").value;
+            var newclock = document.getElementById("clock").value;
+            var dept = document.getElementById("dept").value;
+            var trainer = document.getElementById("train").value;
+            var manag = document.getElementById("manag").value;
+            $.ajax({
+               type: 'post',
+               url: 'emp.php',
+               data: {
+                   	UsersName:name,
+                    NewClock: newclock,
+                    Department: dept,
+                    Trainer: trainer,
+                    Manager: manag,
+               }
+            });
+
+            alert("Employee " + name + " added");
+
+            document.getElementById("uname").value = "";
+            document.getElementById("clock").value = "";
+            document.getElementById("dept").value = "";
+            document.getElementById("train").value = "";
+            document.getElementById("manag").value = "";
+            return true;
+    }
+
+</script>
--- admin.php
+++ admin.php
@@ -5,9 +5,12 @@ include("check.php");
 if($_SESSION['UserID'] > 2){
 	header("Location: home.php");
 }else {
-	
+
 	echo '<h2>Add User</h2>
     <iframe src="UserAddingPage.php" frameborder="0">Your browser does not support iFrames. Please use another browser</iframe>';
+    echo "<br>";
+    echo '<h2>Add Employee</h2>
+    <iframe src="AddEmployee.php" frameborder="0">Your browser does not support iFrames. Please use another browser</iframe>';
 }
 
 ?>

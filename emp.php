d8834de63d509917eb3d336cc88644a0447f1307
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-04-10 08:37:09 +0100

    Added employee adding script

---
+++ emp.php
@@ -0,0 +1,17 @@
+<?php
+include("check.php");
+include("connection.php");
+
+if(!isset($_POST['UsersName'])){
+    header("Location: home.php");
+}else{
+    $name = $_POST['UsersName'];
+    $clock = $_POST['NewClock'];
+    $dept = $_POST['Department'];
+    $train = $_POST['Trainer'];
+    $manag = $_POST['Manag'];
+
+    mysqli_query($connection, "INSERT INTO employee(Name, ClockNo, Department, Trainer, Manager) VALUES ('$name', '$clock', '$dept', '$train', '$manag')");
+
+}
+?>

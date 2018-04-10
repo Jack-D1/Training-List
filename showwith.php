d2cd14e0e0d17eda57fa7f40878634a58ce11641
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-04-10 15:27:39 +0100

    Began adding show with course search page

---
+++ showwith.php
@@ -0,0 +1,22 @@
+<?php
+include("check.php");
+include("connection.php");
+
+$searchterm = "%".$_GET['search']."%";
+
+$startSearch = mysqli_query($connection, "SELECT Name, ClockNo, Department, Trainer, Manager, Course, Level, PassDate, cost FROM course,employee WHERE employee.ClockNo = course.ClockNo AND course.Course LIKE '$searchterm'");
+
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Users With Training Course</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td><b>Name</b></td>
+        </tr>
+    </table>
+</body>
+</html>

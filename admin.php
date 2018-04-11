f85f515eadd3f6787df2b58a727802e5db9d82ae
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-04-11 12:09:23 +0100

    Stlye Changes

--- admin.php
+++ admin.php
@@ -14,6 +14,20 @@ if($_SESSION['UserID'] > 2){
     <link href="stylesheet.css" rel="stylesheet" />
 </head>
 <body>
+    <div id="topbar">
+        <a href="home.php" style="margin-right:15px;">
+            <img src="WPE.jpg" alt="WPE Logo" width="129.25" height="74.74" />
+        </a>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 10px; float: right;">
+            <a href="training.php?clock=<?php echo $Clock['ClockNo'];?>">My Training</a>
+        </h1>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 15px; float: right;">
+            <a href="admin.php">Admin Panel</a>
+        </h1>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 15px; float: right;">
+            <a href="logout.php">Logout</a>
+        </h1>
+    </div>
     <table>
         <tr>
             <td>
--- home.php
+++ home.php
@@ -2,9 +2,8 @@
 include("check.php");
 include("connection.php");
 $UID = $_SESSION['UserID'];
-echo "Welcome back " . $_SESSION['username'];
-echo "<br>";
-echo '<a href = "logout.php">Logout</a>';
+$getClock = mysqli_query($connection, "SELECT ClockNo FROM account WHERE UserID = '$UID'");
+$Clock = mysqli_fetch_assoc($getClock);
 $getManagers = mysqli_query($connection, "SELECT * FROM employee WHERE Manager = 1");
 $checkManager = mysqli_query($connection, "SELECT username FROM account, employee WHERE employee.ClockNo = account.ClockNo AND UserID = '$UID' AND manager = 1");
 if ($_SESSION['UserID'] <= 2){
@@ -18,21 +17,31 @@ if ($_SESSION['UserID'] <= 2){
 </head>
 
 <body>
-    <br>
- 
-    <a href="admin.php">Admin Centre</a>
-    <h3>Managers</h3>
-    <?php
+    <div id="topbar">
+        <a href="home.php" style="margin-right:15px;">
+            <img src="WPE.jpg" alt="WPE Logo" width="129.25" height="74.74" />
+        </a>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 10px; float: right;">
+            <a href="training.php?clock=<?php echo $Clock['ClockNo'];?>">My Training</a>
+        </h1>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 15px; float: right;">
+            <a href="admin.php">Admin Panel</a>
+        </h1>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 15px; float: right;">
+            <a href="logout.php">Logout</a>
+        </h1>
+    </div>
+    <h3>Managers</h3>    <?php
     echo "<table>";
     echo "<tr><td><b>Name</b></td><td><b>Department</b></td><td><b>Clock No</b></td><td><b>Employees</b></td><td><b>Training</b></td></tr>";
     while($managers = mysqli_fetch_assoc($getManagers)){
-        echo "<tr>";
-        echo "<td>".$managers['Name'] ."</td>";
-        echo "<td>".$managers['Department'] ."</td>";
-        echo "<td>".$managers['ClockNo']."</td>";
-        echo '<td><a href = "employees.php?dept='.$managers['Department'].'">View Employees</a></td>';
-        echo '<td><a href = "training.php?clock='.$managers['ClockNo'].'">View training</a></td>';
-        echo "</tr>";
+    echo "<tr>";
+    echo "<td>".$managers['Name'] ."</td>";
+    echo "<td>".$managers['Department'] ."</td>";
+    echo "<td>".$managers['ClockNo']."</td>";
+    echo '<td><a href = "employees.php?dept='.$managers['Department'].'">View Employees</a></td>';
+    echo '<td><a href = "training.php?clock='.$managers['ClockNo'].'">View training</a></td>';
+    echo "</tr>";
     }
     echo "</table>";
     ?>
@@ -51,26 +60,50 @@ if ($_SESSION['UserID'] <= 2){
 ?>
 
 <html>
-<head><title>Employees Training</title></head>
+<head>
+    <title>Employees Training</title>
+    <link href="stylesheet.css" rel="stylesheet" />
+</head>
 <body>
-    <h3>Employees</h3>
+    <div id="topbar">
+        <a href="home.php" style="margin-right:15px;">
+            <img src="WPE.jpg" alt="WPE Logo" width="129.25" height="74.74" />
+        </a>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 10px; float: right;">
+            <a href="training.php?clock=<?php echo $Clock['ClockNo'];?>">My Training</a>
+        </h1>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 10px; float: right;">
+            <a href="logout.php">Logout</a>
+        </h1>
+    </div>
+    <h3>Employees</h3>    
     <?php
     echo "<table>";
     echo "<tr><td><b>Employee</b></td><td><b>Clock No</b></td><td><b>Training</b></td></tr>";
     while($employees = mysqli_fetch_assoc($empFromDep)){
-        echo"<tr>";
-            echo "<td>" . $employees['Name'] . "</td>";
-            echo "<td>".$employees['ClockNo']."</td>";
-            echo '<td><a href = "training.php?clock='.$employees['ClockNo'].'">View training</a></td>';
-        echo"</tr>";
+    echo"<tr>";
+    echo "<td>" . $employees['Name'] . "</td>";
+    echo "<td>".$employees['ClockNo']."</td>";
+    echo '<td><a href = "training.php?clock='.$employees['ClockNo'].'">View training</a></td>';
+    echo"</tr>";
     }
     echo "</table>";
-    ?>
-    <h3>Add Employee</h3>
-    <iframe src="AddEmployee.php" frameborder="0">Your browser does not support iFrames. Please use another browser</iframe>
-
-    <h3>Add Training</h3>
-    <iframe src="addTraining.php" frameborder="0" height=220>Your browser does not support iFrames. Please use another browser</iframe>
+     ?>
+    <br />
+    <table>
+        <tr>
+            <td><h3>Add Employee</h3></td>
+            <td><h3>Add Training</h3></td>
+        </tr>
+        <tr>
+            <td>
+                <iframe src="AddEmployee.php" frameborder="0">Your browser does not support iFrames. Please use another browser</iframe>
+            </td>
+            <td>
+                <iframe src="addTraining.php" frameborder="0" height=220>Your browser does not support iFrames. Please use another browser</iframe>
+            </td>
+        </tr>
+    </table>   
 </body>
 </html>
 
@@ -82,6 +115,17 @@ if ($_SESSION['UserID'] <= 2){
     <title>My Training</title>
 </head>
 <body>
+    <div id="topbar">
+        <a href="home.php" style="margin-right:15px;">
+            <img src="WPE.jpg" alt="WPE Logo" width="129.25" height="74.74" />
+        </a>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 10px; float: right;">
+            <a href="training.php?clock=<?php echo $Clock['ClockNo'];?>">My Training</a>
+        </h1>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 10px; float: right;">
+            <a href="logout.php">Logout</a>
+        </h1>
+    </div>
 </body>
 </html>
 <?php

6beb01143595b2b3680f405fae213ff0fac56cb9
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-04-12 12:25:46 +0100

    style

--- training.php
+++ training.php
@@ -9,14 +9,14 @@ if (!isset($_GET['clock'])){
     $name = mysqli_fetch_assoc($getName);
     $getTraining = mysqli_query($connection, "SELECT Course, Renew, Level, PassDate, Name, cost FROM course,employee WHERE employee.ClockNo = course.ClockNo AND employee.ClockNo = '$clock'");
 
-    echo "<h3>". $name['Name'] . "'s Training</h3>";
-    echo "<table>";
+    echo "<table><tr><td><h3>". $name['Name'] . "'s Training</h3></td></tr>";
+    echo "<tr><td><table>";
     echo "<tr><td><b>Name</b></td><td><b>Course</b></td><td><b>Level</b></td><td><b>Pass Date</b></td><td><b>Renew Date</b></td><td><b>Cost</b></td></tr>";
     while ($training = mysqli_fetch_assoc($getTraining)){
         echo "<tr><td>".$training['Name']."</td><td>".$training['Course']."</td><td>".$training['Level']."</td><td>".$training['PassDate']."</td><td>".$training['Renew']."</td><td>&pound;".$training['cost']."</td></tr>";
     }
 
-    echo "</table>";
+    echo "</table></td></tr></table>";
 }
 ?>
 

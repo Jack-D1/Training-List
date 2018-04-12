3ccbcacb416085d966dd88c71868abf10147a591
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-04-12 16:49:53 +0100

    Style changes

--- employees.php
+++ employees.php
@@ -4,6 +4,21 @@ include("connection.php");
 if ($_SESSION['UserID'] > 2){
     header("Location: home.php");
 }else {
+    echo '<div id="topbar">
+        <a href="home.php" style="margin-right:15px;">
+            <img src="WPE.jpg" alt="WPE Logo" width="129.25" height="74.74" />
+        </a>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 10px; float: right;">
+            <a href="training.php?clock='. $_SESSION['Clock'].'">My Training</a>
+        </h1>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 15px; float: right;">
+            <a href="admin.php">Admin Panel</a>
+        </h1>
+        <h1 style="display:inline-block; margin-bottom: 0; margin-right: 15px; float: right;">
+            <a href="logout.php">Logout</a>
+        </h1>
+    </div>
+    ';
     $dept = $_GET['dept'];
     $deptEmployees = mysqli_query($connection, "SELECT * FROM employee WHERE Department = '$dept'");
     echo "<h3> ". $dept ." Department Employees</h3>";
@@ -24,7 +39,7 @@ echo '<h3>Add Training</h3>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title><?php echo $dept;?> Department Employees</title>
-    <link href="stylehseet.css" rel="stylesheet" />
+    <link href="stylesheet.css" rel="stylesheet" />
 </head>
 <body>
 </body>

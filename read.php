4992b5f3ace2da978639af386c52332882145e8b
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-05-06 21:54:57 +0100

    Effeciecny changes and passing of file name test

--- read.php
+++ read.php
@@ -10,7 +10,8 @@ passed to this script.
 The file name will be passed from the upload script to the function on the GET
 method, 
 */
-$items = file_get_contents($_GET['fileName']);
+$file = '"'.$_GET['fileName'] .'"';
+$items = file_get_contents($file);
 
 /*
 Splitting by EOL and then will split by comma within the loop
@@ -22,12 +23,7 @@ for($i = 0; $i < count($initSplit); $i++){
 	/*This example assumes that there are 3 piece of data associated with
 	eah row, this can be altered for more or less data*/
 	$finSplit = explode(',',$initSplit[$i]);
-	/*Need these variables as the query parses the arrays square brackets */
-	$a = $finSplit[0];
-	$b = $finSplit[1];
-	$c = $finSplit[2];
-	
 	//replace * with table 
-	mysqli_query($connection, "INSERT INTO * VALUES '$a','$b','$c'");
+	mysqli_query($connection, "INSERT INTO * VALUES ".$finSplit[0].",".$finSplit[1].",".$finSplit[2]."");
 }
 ?>

fee51bf5a043d5b52a90e3be6d883a68a1bf3017
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-05-06 16:35:32 +0100

    Changes to file reader, started insertion to database query

--- read.php
+++ read.php
@@ -1,12 +1,16 @@
 <?php
 include("connection.php");
-
 /*
-Test.csv will be replaced with the file that contains employee details, will most 
+From V1 -- Test.csv will be replaced with the file that contains employee details, will most 
 likely be uploaded via form to a folder on the server directory. Its name will be 
 passed to this script.
 */
-$items = file_get_contents("test.csv");
+
+/*
+The file name will be passed from the upload script to the function on the GET
+method, 
+*/
+$items = file_get_contents($_GET['fileName']);
 
 /*
 Splitting by EOL and then will split by comma within the loop
@@ -18,9 +22,12 @@ for($i = 0; $i < count($initSplit); $i++){
 	/*This example assumes that there are 3 piece of data associated with
 	eah row, this can be altered for more or less data*/
 	$finSplit = explode(',',$initSplit[$i]);
-	echo $finSplit[0]." ";
-	echo $finSplit[1]." ";
-	echo $finSplit[2];
-	echo "<br>";
+	/*Need these variables as the query parses the arrays square brackets */
+	$a = $finSplit[0];
+	$b = $finSplit[1];
+	$c = $finSplit[2];
+	
+	//replace * with table 
+	mysqli_query($connection, "INSERT INTO * VALUES '$a','$b','$c'");
 }
 ?>

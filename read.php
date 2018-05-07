d743fdebb00b4bb501a17799de0d2e68ca6864b2
Jack-D1 <jackr_donnelly00@yahoo.com>
2018-05-07 18:08:07 +0100

    Added upload script, added validaiton to file reader

--- read.php
+++ read.php
@@ -1,28 +1,34 @@
 <?php
 include("connection.php");
-/*
-From V1 -- Test.csv will be replaced with the file that contains employee details, will most 
-likely be uploaded via form to a folder on the server directory. Its name will be 
-passed to this script.
-*/
+include("check.php");
 
-/*
-The file name will be passed from the upload script to the function on the GET
-method, 
-*/
-$items = file_get_contents($_GET['fileName']);
+if(!isset($_GET['fileName'])){header("Location: home.php");}else{
+		/*
+		From V1 -- Test.csv will be replaced with the file that contains employee details, will most 
+		likely be uploaded via form to a folder on the server directory. Its name will be 
+		passed to this script.
+		*/
 
-/*
-Splitting by EOL and then will split by comma within the loop
-*/
-$initSplit = explode(PHP_EOL, $items);
+		/*
+		The file name will be passed from the upload script to the function on the GET
+		method, 
+		*/
+		$items = file_get_contents($_GET['fileName']);
+
+		/*
+		Splitting by EOL and then will split by comma within the loop
+		*/
+		$initSplit = explode(PHP_EOL, $items);
 
 
-for($i = 0; $i < count($initSplit); $i++){
-	/*This example assumes that there are 3 piece of data associated with
-	eah row, this can be altered for more or less data*/
-	$finSplit = explode(',',$initSplit[$i]);
-	//replace * with table 
-	mysqli_query($connection, "INSERT INTO * VALUES ".$finSplit[0].",".$finSplit[1].",".$finSplit[2]."");
+		for($i = 0; $i < count($initSplit); $i++){
+			/*This example assumes that there are 3 piece of data associated with
+			eah row, this can be altered for more or less data*/
+			$finSplit = explode(',',$initSplit[$i]);
+			//replace * with table 
+			mysqli_query($connection, "INSERT INTO * VALUES ".$finSplit[0].",".$finSplit[1].",".$finSplit[2]."");
+		}
+
+		header("Location: admin.php");
 }
 ?>
---
+++ upload.php
@@ -0,0 +1,37 @@
+<?php
+include("check.php");
+
+echo '<link rel="stylesheet" href="default.css">';
+
+
+$returnURL = "admin.php";
+$target_dir = "/CSV/";
+$fname = rand().".";
+$target_file = $target_dir . $fname . "csv";
+$imageFileType = strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION));
+$uploadOk = 1;
+if(!isset($_POST["submit"])) {
+    $uploadOk = 0;
+}
+if($imageFileType != "csv") {
+    echo "Sorry, only CSV files are allowed.";
+	echo "<br>";
+    $uploadOk = 0;
+}
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+	echo "<br>";
+	echo "<a href = $returnURL> Click here to return</a>";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		header("Location: read.php?fileName=/CSV/".$fname."csv");
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+		echo "<a href = $returnURL> Click here to return</a>";
+    }
+}
+?>
+
+
+?>
